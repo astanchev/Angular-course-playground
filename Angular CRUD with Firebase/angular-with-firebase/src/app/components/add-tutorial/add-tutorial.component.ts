65c15a149e3ccd9fb74f0754b6b7e9cd67838965
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-11-12 20:58:12 +0200

    Finish app with promises and AngularFireDatabase

--- Angular CRUD with Firebase/angular-with-firebase/src/app/components/add-tutorial/add-tutorial.component.ts
+++ Angular CRUD with Firebase/angular-with-firebase/src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TutorialService } from 'src/app/services/tutorial.service';
+import Tutorial from 'src/app/models/tutorial';
 
 @Component({
   selector: 'app-add-tutorial',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTutorialComponent implements OnInit {
 
-  constructor() { }
+  tutorial: Tutorial = new Tutorial();
+  submitted = false;
+
+  constructor(private tutorialService: TutorialService) { }
 
   ngOnInit(): void {
   }
 
+  saveTutorial(): void {
+    this.tutorialService.create(this.tutorial).then(() => {
+      console.log('Created new item successfully!');
+      this.submitted = true;
+    });
+  }
+
+  newTutorial(): void {
+    this.submitted = false;
+    this.tutorial = new Tutorial();
+  }
 }

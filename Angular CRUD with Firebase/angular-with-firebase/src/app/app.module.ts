d691423a9e531d58063d41da260929496a698c2a
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-11-12 18:32:05 +0200

    Change project name

--- Angular CRUD with Firebase/angular-with-firebase/src/app/app.module.ts
+++ Angular CRUD with Firebase/angular-with-firebase/src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -13,8 +9,6 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
     BrowserModule,
     AppRoutingModule
   ],
